9a4e1854f1213dbc3269c819d46f24a660a8465c
vdienerconsulting-netizen <vdiener.consulting@gmail.com>
2025-10-12 17:08:20 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Presentation from "./components/Presentation";
+import Carte from "./components/Carte";
+import Infos from "./components/Infos";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <Presentation />
+      <Carte />
+      <Infos />
+    </>
+  );
+}
