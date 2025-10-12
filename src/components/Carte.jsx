88f7b8ab94832681f608627f128ca2d739a1e744
vdienerconsulting-netizen <vdiener.consulting@gmail.com>
2025-10-12 19:36:58 +0200

    Update Carte.jsx

--- src/components/Carte.jsx
+++ src/components/Carte.jsx
@@ -6,8 +6,15 @@ export default function Carte() {
 
   const menuItems = {
     "Cocktails": [
-      { n: "Spritz Provençal", d: "Apéritif maison au romarin", p: "9€" },
-      { n: "Sans Alcool Menthe Citron", d: "Rafraîchissant et doux", p: "7€" },
+       { n: "Pomelo Spritz", d: "Apérol, liqueur de pamplemousse maison, rosé pétillant", p: "10€" },
+    { n: "Highball Pomme", d: "Cordial de pomme, eau-de-vie de poire, eau pétillante", p: "8€" },
+    { n: "Almond", d: "Sirop d’orgeat salé, citron vert, rhums du monde", p: "12€" },
+    { n: "Framboise", d: "Cordial framboise maison, vin blanc, pisco", p: "12€" },
+    { n: "Basilic", d: "Tequila basilic, liqueur d’abricot maison, citron vert", p: "10€" },
+    { n: "Highball Pomme 0%", d: "Cordial pomme maison, eau pétillante", p: "6€" },
+    { n: "Floral", d: "Sirop de rose, citron vert, bissap maison", p: "6€" },
+    { n: "Framboise 0%", d: "Cordial framboise, rosé sans alcool, infusion pomme", p: "6€" },
+    { n: "M&T", d: "Melonade, farigoule, eau pétillante", p: "8€" },
     ],
     "Bière & Vin": [
       { n: "Bière artisanale locale", d: "Brasserie de Provence", p: "6€" },
