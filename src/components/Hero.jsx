1ac9b909e6e7fa98c804f18396d1f06b9a30bba6
vdienerconsulting-netizen <vdiener.consulting@gmail.com>
2025-10-13 10:34:29 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
     <section
       id="hero"
-      className="min-h-screen bg-vert text-sable flex flex-col justify-center items-center text-center"
+      className="min-h-screen bg-vert text-sable flex flex-col justify-center items-center text-center overflow-hidden"
     >
-      <h3 className="text-[90px] font-[Bayon] mb-4 tracking-wide">BIENVENUE</h3>
-      <img
+      <motion.h3
+        initial={{ opacity: 0, y: -30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2 }}
+        className="text-[90px] font-[Bayon] mb-4 tracking-wide"
+      >
+        BIENVENUE
+      </motion.h3>
+      <motion.img
         src="/chez-vincent-titre.png"
         alt="Chez Vincent - Buvette Cocktail"
-        className="w-[60%] md:w-[40%] mb-10"
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.8 }}
+        className="w-[60%] md:w-[40%] mt-6"
       />
     </section>
   );
