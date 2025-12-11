787ecc9e2dd847f77cd9cb5a37366ccecce61b6e
vdienerconsulting-netizen <vdiener.consulting@gmail.com>
2025-12-11 10:38:21 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ export default function Hero() {
   return (
     <motion.section
       id="hero"
-      initial={{ y: "15vh", scale: 1.12, opacity: 0 }}
+      initial={{ y: "30vh", scale: 1.3, opacity: 0 }}
       animate={{ y: 0, scale: 1, opacity: 1 }}
-      transition={{ duration: 1.3, ease: "easeInOut" }}
+      transition={{ duration: 1.4, ease: "easeInOut" }}
       className="bg-vert text-sable flex flex-col items-center justify-center text-center overflow-hidden py-16 md:py-20"
     >
       {/* Titre */}
@@ -16,7 +16,7 @@ export default function Hero() {
         alt="Chez Vincent - Buvette Cocktail"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: -10 }}
-        transition={{ duration: 2, delay: 0.4, ease: "easeOut" }}
+        transition={{ duration: 1.6, delay: 0.3, ease: "easeOut" }}
         className="w-[60%] md:w-[40%] mt-2"
       />
 
@@ -26,7 +26,7 @@ export default function Hero() {
         alt="Chez Vincent - sous-titre"
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: -5 }}
-        transition={{ duration: 2, delay: 0.9, ease: "easeOut" }}
+        transition={{ duration: 1.6, delay: 0.8, ease: "easeOut" }}
         className="w-[60%] md:w-[40%] mt-2"
       />
     </motion.section>
