afb0fd6632c52cbba764aea112de1245eaece4b7
vdienerconsulting-netizen <vdiener.consulting@gmail.com>
2025-12-11 09:40:58 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
         initial={{ opacity: 0, y: -40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 2, ease: "easeOut" }}
-        className="text-[90px] font-[Bayon] mb-2 tracking-wide"
+        className="text-[80px] font-[Bayon] mb-2 tracking-wide"
       >
         BIENVENUE
       </motion.h3>
