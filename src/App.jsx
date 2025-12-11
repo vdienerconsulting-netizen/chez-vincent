13024883857691f546cbae68d040057d78d49562
vdienerconsulting-netizen <vdiener.consulting@gmail.com>
2025-12-11 09:39:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Hero from "./components/Hero";
 import Presentation from "./components/Presentation";
 import Carte from "./components/Carte";
 import Contact from "./components/Contact";
-import SideMenu from "./components/SideMenu"; // Menu latéral
 
 function App() {
   return (
     <div className="font-[Raleway] scroll-smooth">
       <Hero />
-      <SideMenu />
       <Presentation />
       <Carte />
       <Contact />
